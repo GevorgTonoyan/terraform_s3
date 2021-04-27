45c49e65733c512f4b4acb4407d63f76c8b56064
Gevorg <32268589+GevorgTonoyan@users.noreply.github.com>
2021-04-27 23:14:34 +0200

    assume role successfull

--- main.tf
+++ main.tf
@@ -1,61 +1,35 @@
-resource "aws_iam_user" "internal_user" {
-  name = "example_internal_employee"
-}
-
-resource "aws_iam_user" "external_user" {
-  name = "example_external_user"
-}
-
-resource "aws_iam_access_key" "internal_user_access_key" {
-  user = "${aws_iam_user.internal_user.name}"
-}
-
-resource "aws_iam_role" "external_users_role" {
-  name = "${var.environment}-external_users_role"
-
-data "template_file" "policy" {
-  template = "${file("${path.module}/assume_role_policy.json")}"
-  
-  vars = {
-    principal = "${aws_iam_role.exterma_users_role.arn}"
-  }
-
 resource "aws_s3_bucket" "important-bucket" {
-	bucket = "${var.environment}-important-stuff"
-    acl = "private"
+  bucket = "${var.environment}-important-stuff"
+  acl    = "private"
 
-    versioning {
-    	enabled = true
-  	}
-
-    lifecycle_rule {
-        enabled = true
-    }
-
-    tags = {
-    	Name = "important-${var.environment}-stuff"
-        Environment = var.environment
-    }
-}
-
-data "template_file" "policy" {
-  template = "${file("${path.module}/${var.environment}_policy.json")}"
-  
-  vars = {
-    bucket_name = "${var.environment}-important-stuff"
-    principal = "${aws_iam_user.internal_user.arn}"
+  versioning {
+    enabled = true
   }
 
+  lifecycle_rule {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "important-${var.environment}-stuff"
+    Environment = var.environment
+  }
 }
 
-resource "aws_iam_policy" "policy" {
-  name        = "${var.environment}-internal_user_policy"
-  description = "My test policy"
-  policy = "${data.template_file.policy.rendered}"
-}
+resource "aws_s3_bucket" "contractors-bucket" {
+  bucket = "${var.environment}-contractor-development"
+  acl    = "private"
 
-resource "aws_iam_user_policy" "internal_user_bucket_access" {
-  name = "internal_users_bucket_policy"
-  user = "${aws_iam_user.internal_user.name}"
-  policy = "${data.template_file.policy.rendered}"
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+  }
+
+  tags = {
+    Name        = "${var.environment}-contractor-development"
+    Environment = var.environment
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,23 @@
 variable "region" {
   type        = string
   description = "default aws region"
-  default = "eu-central-1"
+  default     = "eu-central-1"
 }
 
 variable "environment" {
   type        = string
   description = "Environment name to create."
-  default =  "dev"
+  default     = "dev"
+}
+
+variable "account_id" {
+  description = "AWS Account ID that is allowed to assume this role."
+  type        = string
+  default     = "12345678"
+}
+
+variable "external_id" {
+  type        = string
+  description = "External ID provided by third party."
+  default     = "12345"
 }
